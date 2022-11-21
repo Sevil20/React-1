db4cc9bc1fc8f8dcab3574f346de9199d8f82757
Sevil20 <sevilmirzayeva010@gmail.com>
2022-11-21 15:32:51 +0400

    sdfsdf

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+     <h1 style={{color:red}}>Sevil Mirzayeva</h1>
+    </div>
+  )
+}
+
+export default App
